3248126877aba3794ca2ee20417db343009a621a
zhangchengxin <zhangchengxin@meicai.cn>
2019-11-20 13:23:49 +0800

    compat: rem

--- compat/rem.js
+++ compat/rem.js
@@ -2,9 +2,10 @@
  * 通过 rem 使页面和等比例缩放
  * 
  * 实现逻辑
- * 1. 通过公式 rem / clientWidth = 100 / 设计图宽，得出 rem.
+ * 1. 将基数设置成 100，通过公式 rem / clientWidth = 100 / 设计图宽，得出 rem.
  * 2. css 中的值就是 实际的长度 / 100，加上单位 rem. 如：
- *  div { width: 1rem } 1rem = (设计图的实际尺寸 / 100)rem
+ *  div { width: 1rem } 1rem = (设计图的实际尺寸 / 100)rem.
+ *  通过将基数设置成 100，可以方便的计算出 css 的大小
  *
  * @param {number?} width 	设计图宽
  * @param {number?} height 	设计图宽
