5229792e4e9238db76e2bad925003c8f868af672
Katt Zhang <1193055507@qq.com>
2019-12-24 20:59:40 +0800

    format compat:rem

--- src/compat/rem.js
+++ src/compat/rem.js
@@ -7,8 +7,8 @@
  *  div { width: 1rem } 1rem = (设计图的实际尺寸 / 100)rem.
  *  通过将基数设置成 100，可以方便的计算出 css 的大小
  *
- * @param {number?} width 	设计图宽
- * @param {number?} height 	设计图高
+ * @param {number?} width   设计图宽
+ * @param {number?} height  设计图高
  * @param {number?} max     最大宽 (达到时不再缩放)
  * 
  */
