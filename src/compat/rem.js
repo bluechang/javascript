ac2f7d2f7cd5ab6638fccd9d57f57609f449f4c2
Katt Zhang <1193055507@qq.com>
2019-12-24 21:02:27 +0800

    format compat:rem

--- src/compat/rem.js
+++ src/compat/rem.js
@@ -19,26 +19,26 @@ export default function rem (width, height, max) {
   let design, client
 
   if (typeof width === 'number' && !height) {
-    design = width;
+    design = width
   }
 
   if (!width && typeof height === 'number') {
-    design = height;
+    design = height
   }
 
   function handleResize() {
-    client = docEl[width ? 'clientWidth' : 'clientHeight'];
+    client = docEl[width ? 'clientWidth' : 'clientHeight']
 
     // 以宽为标准，达到最大尺寸时，不变
     if (width && (max || (max = width)) && (client >= max)) {
-      client = max;
+      client = max
     }
 
-    docEl.style.fontSize = (base / design) * client + 'px';
+    docEl.style.fontSize = (base / design) * client + 'px'
   }
 
-  window.addEventListener('resize', handleResize, false);
+  window.addEventListener('resize', handleResize, false)
 
-  handleResize();
+  handleResize()
 }
 
