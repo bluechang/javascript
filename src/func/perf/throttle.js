cfc1ebadc19ddef7e89a56223a4f739774bbe78a
Katt Zhang <1193055507@qq.com>
2020-04-01 15:00:13 +0800

    update throttle.js

--- src/func/perf/throttle.js
+++ src/func/perf/throttle.js
@@ -1,6 +1,5 @@
 /**
  * throttle
- * 时间戳会比定时器更准确
  * 
  * 场景：如验证码、拖拽
  * 
@@ -8,16 +7,34 @@
  * @param {number} wait   延迟执行的时间
  * 
  */
+
+// 时间版：时间戳会比定时器更准确
 export function throttle(fn, wait = 50) {
   let prev = 0
 
-  return function(...args) {
+  return function() {
     const context = this
     let now = Date.now()
 
     if (now - prev >= wait) {
-      fn.apply(context, args)
+      fn.apply(context, arguments)
       prev = now
     }
   }
 }
+
+// 定时器版
+// export function throttle(fn, wait = 50) {
+//   let timer = null
+  
+//   return function() {
+//     const context = this
+    
+//     if (timer) return
+  
+//     timer = setTimeout(() => {
+//       fn.apply(context, arguments)
+//       timer = null
+//     }, wait)
+//   }
+// }
